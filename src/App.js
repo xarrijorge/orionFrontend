dd7ead74f1a23d0258e3b7e7098e814543965f0f
KarenWanyama3430 <karenwanyama3430@gmail.com>
2024-03-10 14:13:44 +0300

    changes to step 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import OnboardingPage from './pages/OnboardingPage';
 import Signup from './components/onboarding/Signup';
 import Instructions from './components/onboarding/Instructions';
 import Step1 from './components/onboarding/Step1';
+import Step2 from './components/onboarding/Step2';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Signup/>}/>
         <Route path="/instructions" element={<Instructions/>}/>
         <Route path="/step_one" element={<Step1/>}/>
+        <Route path="/step_two" element={<Step2/>}/>
         </Route>
       </Routes>
     </Router>
