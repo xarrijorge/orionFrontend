748407cb6baf209ea43958207161f4f45e585e2c
KarenWanyama3430 <karenwanyama3430@gmail.com>
2024-03-10 11:34:52 +0300

    changes to the instructions screen

--- src/components/onboarding/Instructions.jsx
+++ src/components/onboarding/Instructions.jsx
@@ -2,10 +2,22 @@ import React, { useState } from 'react'
 import CardContent from '@mui/material/CardContent';
 import "./Onboarding.css"
 import logo from "../../assets/logo .png"
-import Stack from '@mui/material/Stack'; 
 import Button from '@mui/material/Button';
 import { Link, useNavigate } from 'react-router-dom';
-import Item from '@mui/material/Item';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import property from "../../assets/property.svg";
+import units from "../../assets/units.svg";
+import technician from "../../assets/technician.svg";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const Instructions = () => {
   return (
@@ -20,16 +32,46 @@ const Instructions = () => {
         </p>
 
     </CardContent>
-    <Stack direction="row" spacing={2}>
-        <Item>
+    <div className='flex'>
+        <div className='flex-column'>
             <Stack spacing={2}>
-
+            <Button type='submit'  className= 'signup_button' variant="outlined">Step 1</Button>
+            <div className='card2'>
+                <img src={property} alt="property" />
+                <h4>Add a Property</h4>
+                <p className='grey'>Start by adding your property to NyumbaFix. Provide details such as property name, address, and type.</p>
+            </div>
             </Stack>
-        </Item>
+        </div>
 
+        <div className='flex-column'>
+            <Stack spacing={2}>
+            <Button type='submit'  className= 'signup_button' variant="outlined">Step 2</Button>
+            <div className='card2'>
+                <img src={units} alt="property" />
+                <h4>Add a Property</h4>
+                <p className='grey'>Once your property is added, you'll proceed to add units within it.</p>
+            </div>
+            </Stack>
+        </div>
 
+        <div className='flex-column'>
+            <Stack spacing={2}>
+            <Button type='submit'  className= 'signup_button' variant="outlined">Step 3</Button>
+            <div className='card2'>
+                <img src={technician} alt="property" />
+                <h4>Add a Property</h4>
+                <p className='grey'>Finally, add technicians who will assist in maintaining your properties. Specify their specialties and contact information.</p>
+            </div>
+            </Stack>
+        </div>
 
-    </Stack>
+    </div>
+
+    <CardContent className='Welcome'>
+    <Link to='/step_one' className='no_deco'><Button type='submit'  className= 'signup_button' variant="contained">Begin Onboarding</Button></Link><br/><br/>
+    <Link to='/loading' className='black'>Skip for now</Link>
+    </CardContent>
 
    
 </Stack>
