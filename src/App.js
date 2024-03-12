457bfd37ff529e9a5eb843333e0a53b683f83637
Xarri Jorge <kodinglife@gmail.com>
2024-03-12 22:59:39 +0000

    Fixed routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Signup from './components/onboarding/Signup';
 import Instructions from './components/onboarding/Instructions';
 import Layout from './components/layout/layout'; // Import the Layout component
 import DashboardHome from './pages/dashboard/DashboardHome'; // Import the DashboardHome component
+import RepairRequests from './pages/dashboard/RepairRequests'
+import WorkOrders from './pages/dashboard/WorkOrders'
+import Properties from './pages/dashboard/Properties';
 import Step1 from './components/onboarding/Step1';
 import Step2 from './components/onboarding/Step2';
 
@@ -19,8 +22,17 @@ function App() {
           <Route path="/step_one" element={<Step1 />} />
           <Route path="/step_two" element={<Step2 />} />
         </Route>
-        <Route path="/dashboard/home" element={<Layout><DashboardHome /></Layout>}> {/* Layout for dashboard sub-routes */}
+        <Route path="/dashboard/home" element={<Layout><DashboardHome /></Layout>}>
         </Route>
+        <Route path="/dashboard/requests" element={<Layout><RepairRequests /></Layout>}>
+        </Route>
+        <Route path="/dashboard/orders" element={<Layout><WorkOrders /></Layout>}>
+        </Route>
+        <Route path="/dashboard/properties" element={<Layout><Properties /></Layout>}>
+        </Route>
+
+
+
       </Routes>
     </Router >
   );
