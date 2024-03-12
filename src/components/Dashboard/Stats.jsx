288cee9db4a1ae9042a992e4e87e6e464e5eaa3e
Xarri Jorge <kodinglife@gmail.com>
2024-03-12 19:50:17 +0000

    Made stats component reusable

--- src/components/Dashboard/Stats.jsx
+++ src/components/Dashboard/Stats.jsx
@@ -3,9 +3,10 @@ import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
 
-const MaintenanceStats = () => {
+const MaintenanceStats = (arr = []) => {
   return (
     <>
+<<<<<<< Updated upstream
       <Paper>
         <Grid container xs={12}>
           <Grid xs={3}>
@@ -33,6 +34,30 @@ const MaintenanceStats = () => {
             <Typography variant='h6'>Nothing to show yet</Typography>
           </Grid>
         </Grid>
+=======
+      <Paper
+        style={{
+          padding: '20px',
+          minHeight: '100px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}>
+        {arr.map((el, idx) => (
+          // eslint-disable-next-line react/jsx-key
+          <Grid container xs={12}>
+            <Grid xs={3} key={idx}>
+              <Typography variant='h5' style={{ color: '#00B286' }}>
+                {el.title}
+              </Typography>
+              <Typography variant='h6' style={{ color: '#868E96' }}>
+                {el.subtitle}
+              </Typography>
+            </Grid>
+          </Grid>
+        ))}
+>>>>>>> Stashed changes
       </Paper>
     </>
   )
