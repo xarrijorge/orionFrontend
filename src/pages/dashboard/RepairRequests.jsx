6247d7032427536e4cd336e45f11dc7d38c3c5e3
Xarri Jorge <kodinglife@gmail.com>
2024-03-14 22:31:23 +0000

    Updated pages with new design

--- src/pages/dashboard/RepairRequests.jsx
+++ src/pages/dashboard/RepairRequests.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import UnitsTable from '../../components/UnitTable'
 import Box from '@mui/material/Box'
+import ActionNav from '../../components/ActionNav'
+import AppsIcon from '@mui/icons-material/Apps'
+import List from '@mui/icons-material/List'
 
 const RepairRequests = () => {
   const dummyData = [
@@ -13,37 +17,44 @@ const RepairRequests = () => {
           unitNumber: 'A101',
           request: 'Repair plumbing',
           status: 'Pending',
+          title: 'Request title',
         },
         {
           id: 2,
           unitNumber: 'B205',
           request: 'Fix electrical issue',
           status: 'In Progress',
+          title: 'Request title',
         },
         {
           id: 3,
           unitNumber: 'C304',
           request: 'Paint walls',
           status: 'Completed',
+          title: 'Request title',
         },
         {
           id: 4,
           unitNumber: 'D403',
           request: 'Replace light fixtures',
           status: 'Pending',
+          title: 'Request title',
         },
         {
           id: 5,
           unitNumber: 'E502',
           request: 'Repair HVAC',
           status: 'Pending',
+          title: 'Request title',
         },
       ],
     },
   ]
 
+  const icons = [<List />, <AppsIcon />]
   return (
     <>
+      <ActionNav title='Repair requests' icons={icons} />
       <Box>
         <UnitsTable data={dummyData} />
       </Box>
