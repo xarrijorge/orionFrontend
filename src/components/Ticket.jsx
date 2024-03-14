f6e9d7a120734f4fd931dfe9989214e63f384ad0
Xarri Jorge <kodinglife@gmail.com>
2024-03-14 22:30:37 +0000

    Fixed tickets component

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,7 +2,20 @@
 import React from 'react'
 import { Card, CardContent, Typography, Box, Chip } from '@mui/material'
 
-const TicketComponent = ({ ticketNumber, description, status, date }) => {
+const colors = {
+  new: '#7A7B7C',
+  done: '#3CA38F',
+  progress: 'orange',
+  cancelled: 'red',
+}
+
+const TicketComponent = ({
+  ticketNumber,
+  description,
+  status,
+  date,
+  title,
+}) => {
   return (
     <Card sx={{ mb: 2 }}>
       <CardContent>
@@ -10,13 +23,21 @@ const TicketComponent = ({ ticketNumber, description, status, date }) => {
           <Typography variant='h6' gutterBottom>
             Ticket #{ticketNumber}
           </Typography>
-          <Chip label={status} color='success' size='small' />
+          <Chip
+            label={status === 'progress' ? 'IN PROGRESS' : status.toUpperCase()}
+            sx={{
+              backgroundColor: colors[status],
+              color: '#fff',
+              fontSize: 10,
+            }}
+            size='small'
+          />
         </Box>
         <Typography variant='body2' gutterBottom>
           {description}
         </Typography>
         <Box display='flex' justifyContent='space-between' alignItems='center'>
-          <Chip label={status} color='success' size='small' />
+          <Chip label={title} size='small' />
           <Typography variant='body2'>{date}</Typography>
         </Box>
       </CardContent>
